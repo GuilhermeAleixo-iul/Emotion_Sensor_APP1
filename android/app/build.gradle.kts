9c33197b1ca138edc01cca0fb63a1af7f15fb4cf
robert <robertnganga220@gmail.com>
2025-06-18 19:20:53 +0300

    Update project configuration and dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,6 +39,22 @@ android {
     }
 }
 
+
+dependencies {
+    implementation("com.shimmerresearch:shimmerbluetoothmanager:0.11.3_beta") {
+        exclude(group = "io.netty")
+        exclude(group = "com.google.protobuf")
+        exclude(group = "org.apache.commons.math")
+    }
+
+    implementation("com.shimmerresearch:shimmerdriver:0.11.3_beta") {
+        exclude(group = "io.netty")
+        exclude(group = "com.google.protobuf")
+    }
+
+    implementation("com.shimmerresearch:shimmerandroidinstrumentdriver:3.2.2_beta@aar")
+}
+
 flutter {
     source = "../.."
 }
